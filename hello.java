cb6562dae3c51bf43741b95eb547ef98ee5a3824
peaceguard <marek.stoppel@gmail.com>
2021-03-23 06:21:51 +0100

    First Java project: Hello World!

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
